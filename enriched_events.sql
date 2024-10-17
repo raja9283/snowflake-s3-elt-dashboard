ccd375fc1c9b0f495b2361bc387f36603796d18a
Rajendra Patel <rajendrapatel9283@gmail.com>
2024-10-17 14:30:35 +0530

    git check

--- enriched_events.sql
+++ enriched_events.sql
@@ -24,6 +24,10 @@ SELECT
     products.subcategory,
     customers.customer_name,
     customers.location,
+sdd
+sd
+    dfdf
+    dfdf
     customers.AGE_GROUP,
 FROM events AS events
 LEFT JOIN products
